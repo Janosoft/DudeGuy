b710360d4e2f5c691880c5e196f5acc2645fd12c
Alejandro Lodes <janosoft@gmail.com>
2024-02-27 19:47:17 -0300

    territory type

--- Scripts/Places/TestEnvironment.gd
+++ Scripts/Places/TestEnvironment.gd
@@ -1,8 +1,12 @@
 extends Node2D
 
 @onready var dude_guy = $DudeGuy
+const territoryTypes = {'HOSTILE': -5, 'NEUTRAL': 0, 'FRIENDLY': 5}
+var territoryType = territoryTypes.get('NEUTRAL')
 var temperature = 5
 
 func _ready():
+	dude_guy.setPerception(territoryType)
 	dude_guy.setTemperature(temperature)
 	dude_guy.talk("Hello Moto")
+	
