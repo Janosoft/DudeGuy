88fc15abc2820f35fe3afd490183fdac38f09895
Alejandro Lodes <janosoft@gmail.com>
2024-02-28 19:44:57 -0300

    Check objects and react near things

--- Scripts/DudeGuy/DudeGuy.gd
+++ Scripts/DudeGuy/DudeGuy.gd
@@ -1,14 +1,15 @@
-extends Sprite2D
+extends CharacterBody2D
 
 @onready var _body = $Body
 
 #region Status
 var _perception : int = 0
-var _temperature : int = 0
-var _aggressiveness : int = 0
+var temperature : int = 0
+var aggressiveness : int = 0
 #endregion
 
-var _actualEmotion: String = 'default'
+var _actualEmotion : String = 'default'
+var _lastTemperature : int = 0
 
 func _ready():
 	emotion(_actualEmotion)
@@ -20,9 +21,29 @@ func emotion(newEmotion: String):
 	_body.emotion(newEmotion)
 
 func setTemperature(newTemperature: int):
-	_temperature= newTemperature
-	_body.setTemperature(_temperature)
+	_lastTemperature= temperature
+	temperature= newTemperature
+	_body.setTemperature(temperature)
 
 func setPerception(newPerception: int):
 	#It's diferent interact to something in a hostile place than a friendly one
 	_perception= newPerception
+
+func checkObject(thing: Object):
+	if ('aggressiveness' in thing):
+		if (aggressiveness - (thing.aggressiveness + _perception) < 0):
+			print_debug(aggressiveness - (thing.aggressiveness + _perception))
+			emotion("Scared")
+			talk("Ohh my God")
+	else:
+		print_debug('Not aggressiveness')
+
+func _on_hitbox_body_entered(body):
+	print_debug("Trasspassing: " + body.name)
+	if ('temperature' in body):
+		setTemperature(body.temperature)
+
+func _on_hitbox_body_exited(body):
+	print_debug("Leaving: " + body.name)
+	if ('temperature' in body):
+		setTemperature(_lastTemperature)
