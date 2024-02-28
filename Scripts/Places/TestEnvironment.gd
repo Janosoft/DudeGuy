2378c92e033b37788edbb4bb0f240148bb24acee
Alejandro Lodes <janosoft@gmail.com>
2024-02-28 19:47:14 -0300

    less orders more autonomy

--- Scripts/Places/TestEnvironment.gd
+++ Scripts/Places/TestEnvironment.gd
@@ -1,12 +1,10 @@
 extends Node2D
 
 @onready var dude_guy = $DudeGuy
-const territoryTypes = {'HOSTILE': -5, 'NEUTRAL': 0, 'FRIENDLY': 5}
+const territoryTypes = {'HOSTILE': -1, 'NEUTRAL': 0, 'FRIENDLY': 1}
 var territoryType = territoryTypes.get('NEUTRAL')
 var temperature = 0
 
 func _ready():
 	dude_guy.setPerception(territoryType)
 	dude_guy.setTemperature(temperature)
-	dude_guy.emotion('Love')
-	dude_guy.talk("Hello Moto")
