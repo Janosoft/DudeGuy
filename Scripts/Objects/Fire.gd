d2991c9a83ad66140d594eed992a18761b4150a9
Alejandro Lodes <janosoft@gmail.com>
2024-02-29 20:42:58 -0300

    status now is a dictionary

--- Scripts/Objects/Fire.gd
+++ Scripts/Objects/Fire.gd
@@ -1,12 +1,11 @@
 extends CharacterBody2D
 
 #region Status
-var temperature : int = 5
-var aggressiveness : int = 0
+var status = {'Temperature' : 5}
 #endregion
 
 const SPEED = -450.0
 
 func _physics_process(delta):
 	velocity.x = SPEED * delta
-	move_and_slide()
+	#move_and_slide()
--- Scripts/Objects/FurryMonster.gd
+++ Scripts/Objects/FurryMonster.gd
@@ -1,8 +1,7 @@
 extends CharacterBody2D
 
 #region Status
-var temperature : int = 0
-var aggressiveness : int = 5
+var status = {'Aggressiveness' : 5}
 #endregion
 
 const SPEED = -300.0
