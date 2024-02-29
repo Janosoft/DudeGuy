be41e8eb3c31ba30dda5b39dca5c44527bf3b3c0
Alejandro Lodes <janosoft@gmail.com>
2024-02-29 20:42:45 -0300

    better handling of emotions

--- Scripts/DudeGuy/DudeGuy.gd
+++ Scripts/DudeGuy/DudeGuy.gd
@@ -9,16 +9,19 @@ var aggressiveness : int = 0
 #endregion
 
 var _actualEmotion : String = 'default'
+var _lastEmotion : String = 'default'
 var _lastTemperature : int = 0
 
 func _ready():
-	emotion(_actualEmotion)
+	setEmotion(_actualEmotion)
 	
 func talk(words: String):
 	_body.talk(words)
 
-func emotion(newEmotion: String):
-	_body.emotion(newEmotion)
+func setEmotion(newEmotion: String):
+	_lastEmotion = _actualEmotion
+	_body.setEmotion(newEmotion)
+	$EmotionTimer.start()
 
 func setTemperature(newTemperature: int):
 	_lastTemperature= temperature
@@ -29,21 +32,51 @@ func setPerception(newPerception: int):
 	#It's diferent interact to something in a hostile place than a friendly one
 	_perception= newPerception
 
-func checkObject(thing: Object):
-	if ('aggressiveness' in thing):
-		if (aggressiveness - (thing.aggressiveness + _perception) < 0):
-			print_debug(aggressiveness - (thing.aggressiveness + _perception))
-			emotion("Scared")
-			talk("Ohh my God")
-	else:
-		print_debug('Not aggressiveness')
+func emotionCalculator(thingStatus: Dictionary)-> String :
+	var maxStatus = ''
+	var maxValue = -5
+	var emotion = 'default'
+	
+	for key in thingStatus.keys():
+		if (thingStatus[key] > maxValue):
+			maxStatus = key
+			maxValue = thingStatus[key]
+	
+	match maxStatus:
+		'Striking':
+			emotion = "Wonder"
+		'Aggressiveness':
+			if (aggressiveness - (maxValue + _perception) < 0):
+				emotion = 'Scared'
+		_:
+			emotion = 'default'
+	
+	return emotion
 
+func talkCalculator(thingStatus: Dictionary)-> String :
+	return 'Ohh my God'
+
+func checkObject(thing: Object):
+	var newEmotion:String = emotionCalculator(thing.status)
+	var words:String = talkCalculator(thing.status)
+	setEmotion(newEmotion)
+	if (!words.is_empty()) :talk(words)
+
+#region Catch Signals#
 func _on_hitbox_body_entered(body):
-	print_debug("Trasspassing: " + body.name)
+	#print_debug("Trasspassing: " + body.name)
 	if ('temperature' in body):
-		setTemperature(body.temperature)
+		if (body.temperature != 0): setTemperature(body.temperature)
+	if ('aggressiveness' in body):
+		if (body.aggressiveness > 0): setEmotion('Pain')
 
 func _on_hitbox_body_exited(body):
-	print_debug("Leaving: " + body.name)
+	#print_debug("Leaving: " + body.name)
 	if ('temperature' in body):
-		setTemperature(_lastTemperature)
+		if (body.temperature != 0): setTemperature(_lastTemperature)
+	if ('aggressiveness' in body):
+		if (body.aggressiveness > 0): setEmotion(_lastEmotion)
+#endregion
+
+func _on_emotion_timer_timeout():
+	setEmotion('default')
