256f9629096734bd0cccf53ed536407e31a7d99c
Alejandro Lodes <janosoft@gmail.com>
2024-03-01 17:21:25 -0300

    gira el personaje

--- Scripts/Places/MarioWorld.gd
+++ Scripts/Places/MarioWorld.gd
@@ -7,7 +7,8 @@ const SPEED = 10
 const MAXSPEED = 125
 const JUMP_VELOCITY = 350.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var direction = 0
+var direction = 1
+var lastDirection = 1
 var currentSpeed = 1
 #endregion
 
@@ -24,8 +25,11 @@ func _controls():
 	if dude_guy.is_on_floor():
 		if Input.is_action_just_pressed("jump"):
 			dude_guy.velocity.y = - JUMP_VELOCITY
-	direction = Input.get_axis("move_left", "move_right")
+	direction = Input.get_axis("move_left", "move_right")	
 	if direction:
+		if lastDirection != direction:
+			lastDirection = direction
+			dude_guy.scale.x=-1
 		dude_guy.velocity.x =  max(dude_guy.velocity.x - SPEED, -MAXSPEED) * currentSpeed if direction<0 else min(dude_guy.velocity.x + SPEED, MAXSPEED) * currentSpeed
 	else:
 		dude_guy.velocity.x = lerp(dude_guy.velocity.x,0.0,0.2)
