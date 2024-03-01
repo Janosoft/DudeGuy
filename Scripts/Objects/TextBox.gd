545302a6d617ef8ffa564a9b5057f16c21674ac0
Alejandro Lodes <janosoft@gmail.com>
2024-03-01 19:50:15 -0300

    wait time last text length

--- Scripts/Objects/TextBox.gd
+++ Scripts/Objects/TextBox.gd
@@ -2,13 +2,11 @@ extends CharacterBody2D
 @onready var label = $Label
 @onready var timer = $Timer
 
-func setText(newText:String, time: int):
+func setText(newText:String):
 	label.text = newText
-	timer.wait_time = time;
+	timer.wait_time = len(newText) * 0.17
 	visible = true
 	timer.start()
-
-
-
+	
 func _on_timer_timeout():
 	visible= false;
