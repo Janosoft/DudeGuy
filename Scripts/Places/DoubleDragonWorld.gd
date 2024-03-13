5245e8d80d95d3e9c3f5976b16d48a13066bc5fe
Alejandro Lodes <janosoft@gmail.com>
2024-03-12 23:47:53 -0300

    BillyLee fused with DudeGuy

--- Scripts/Places/DoubleDragonWorld.gd
+++ Scripts/Places/DoubleDragonWorld.gd
@@ -6,6 +6,7 @@ signal gameOver
 
 #region Privated Variables
 @onready var _dude_guy = $DudeGuy
+@onready var billy_lee = $BillyLee
 @onready var _background = $Background
 const _SPEED = 10
 const _MAXSPEED = 125
@@ -43,23 +44,38 @@ func _ready():
 func _physics_process(delta):
 	_controls()
 	_dude_guy.move_and_slide()
+	billy_lee.move_and_slide()
 
 func _controls():
+	#TODO FIX DIAGONALS DO SCALE WRONG
 	_direction = Input.get_vector("move_left","move_right","move_up","move_down")
 	if (_direction.x):
 		if (_lastDirection != _direction.x):
 			_lastDirection = _direction.x
 			_dude_guy.scale.x=-1
+			billy_lee.scale.x=-2
 		_dude_guy.velocity.x =  max(_dude_guy.velocity.x - _SPEED, -_MAXSPEED) * _currentSpeed if _direction.x<0 else min(_dude_guy.velocity.x + _SPEED, _MAXSPEED) * _currentSpeed
+		billy_lee.velocity.x =  max(billy_lee.velocity.x - _SPEED, -_MAXSPEED) * _currentSpeed if _direction.x<0 else min(billy_lee.velocity.x + _SPEED, _MAXSPEED) * _currentSpeed
 	else:
 		_dude_guy.velocity.x = lerp(_dude_guy.velocity.x,0.0,0.2)
+		billy_lee.velocity.x = lerp(billy_lee.velocity.x,0.0,0.2)
 	
 	if (_direction.y):
 		_dude_guy.velocity.y =  max(_dude_guy.velocity.y - _SPEED, -_MAXSPEED) * _currentSpeed if _direction.y<0 else min(_dude_guy.velocity.y + _SPEED, _MAXSPEED) * _currentSpeed
+		billy_lee.velocity.y =  max(billy_lee.velocity.y - _SPEED, -_MAXSPEED) * _currentSpeed if _direction.y<0 else min(billy_lee.velocity.y + _SPEED, _MAXSPEED) * _currentSpeed
 	else:
 		_dude_guy.velocity.y = lerp(_dude_guy.velocity.y,0.0,0.2)
+		billy_lee.velocity.y = lerp(billy_lee.velocity.y,0.0,0.2)
 	
 	#Limits the movements inside the level
 	_dude_guy.position.x = clamp(_dude_guy.position.x, 17, _background.texture.get_size().x-50)
 	_dude_guy.position.y = clamp(_dude_guy.position.y, 116, _background.texture.get_size().y)
+	billy_lee.position.x = clamp(billy_lee.position.x, 17, _background.texture.get_size().x-50)
+	billy_lee.position.y = clamp(billy_lee.position.y, 113, _background.texture.get_size().y-3)
 	
+	if Input.is_action_just_pressed("fire"):
+		billy_lee.hit()
+
+func _on_billy_lee_enemy_hit(body):
+	_dude_guy.setEmotion("Happy")
+	body.getHit(billy_lee.position.x - body.position.x)
