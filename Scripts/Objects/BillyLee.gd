a114010130624985cddf1d47a26783908fb665de
Alejandro Lodes <janosoft@gmail.com>
2024-03-12 23:48:19 -0300

    Billy Lee hits

--- Scripts/Objects/BillyLee.gd
+++ Scripts/Objects/BillyLee.gd
@@ -1,2 +1,24 @@
 extends CharacterBody2D
 
+#region Public Variables
+signal enemyHit
+#endregion
+
+#region Privated Variables
+@onready var _animated_sprite_2d = $AnimatedSprite2D
+@onready var _hitbox_collision_shape_2d = $HitBox/CollisionShape2D
+@onready var _hitbox_timer = $HitBox/HitboxTimer
+#endregion
+
+func hit():
+	if (!_animated_sprite_2d.is_playing()): 
+		_animated_sprite_2d.play("hit_" + str(randi() % 2 + 1))
+		_hitbox_collision_shape_2d.set_deferred("disabled", false)
+		_hitbox_timer.start()
+
+func _on_hit_box_body_entered(body):
+	emit_signal("enemyHit", body)
+	_hitbox_collision_shape_2d.set_deferred("disabled", true)
+
+func _on_hitbox_timer_timeout():
+	_hitbox_collision_shape_2d.set_deferred("disabled", true)
